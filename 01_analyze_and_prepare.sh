54bef9746dafb76669a2a665d99c95dce9197202
GoldenStiv-Fedora <goldenstiv@gmail.com>
2025-04-28 21:56:05 +0300

    Очистка файла 01_analyze_and_prepare.sh

--- 01_analyze_and_prepare.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Анализ без привязки к пользователю
-CPU_VENDOR=$(grep -m1 "vendor_id" "$LOG_DIR/lscpu.log" | cut -d: -f2 | tr -d '[:space:]')
-echo "cpu_vendor=$CPU_VENDOR" > /tmp/system_config_detected.conf
